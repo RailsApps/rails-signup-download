ba20f1a6f6d08d7ab4b9c71077126630111dc324
Daniel Kehoe <daniel@danielkehoe.com>
2014-04-28 13:22:54 -0700

    home page feature test

---
+++ spec/features/visitor/home_page_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+# Feature: Home Page
+#   As a visitor
+#   I want to visit a home page
+#   So I can learn more about the website
+feature 'Home Page:' do
+
+  # Scenario: Visit the Home Page
+  #   Given I am a visitor
+  #   When I visit the home page
+  #   Then I should see "Home"
+  scenario 'Visit the Home Page' do
+
+  end
+
+end
