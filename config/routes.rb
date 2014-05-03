78ebbe5f88579935b845398b47eda41409a6beef
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-03 15:26:50 -0700

    rename home controller as visitors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RailsSignupDownload::Application.routes.draw do
-  root :to => "home#index"
+  root :to => "visitors#index"
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
