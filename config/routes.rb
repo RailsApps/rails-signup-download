8e0f774c9872b1bfdc44e21dcc78e5096f479dae
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-11 08:34:49 -0700

    build using rails_apps_composer 2.6.6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => "visitors#index"
   get 'products/:id', to: 'products#show', :as => :products
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users
   resources :users
 end
